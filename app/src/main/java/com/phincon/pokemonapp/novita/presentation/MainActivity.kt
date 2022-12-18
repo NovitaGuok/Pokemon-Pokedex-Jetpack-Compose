0d8195a03310a4cdf50a9f8015ce10fd1d8e9895
NovitaGuok <novitaguok@gmail.com>
2022-12-18 11:48:00 +0700

    Add libraries and some basic configurations

--- app/src/main/java/com/phincon/pokemonapp/novita/presentation/MainActivity.kt
+++ app/src/main/java/com/phincon/pokemonapp/novita/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.phincon.pokemonapp.novita
+package com.phincon.pokemonapp.novita.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,8 +10,10 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.phincon.pokemonapp.novita.ui.theme.PhinConTechnicalTestTheme
+import com.phincon.pokemonapp.novita.presentation.ui.theme.PhinConTechnicalTestTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/phincon/pokemonapp/novita/presentation/ui/theme/Shape.kt
+++ app/src/main/java/com/phincon/pokemonapp/novita/presentation/ui/theme/Shape.kt
@@ -1,4 +1,4 @@
-package com.phincon.pokemonapp.novita.ui.theme
+package com.phincon.pokemonapp.novita.presentation.ui.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
